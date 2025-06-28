317cd0ac8a75d3f6d237e300c1744190207ed43c
tinnitrom <156571351+tinnitrom@users.noreply.github.com>
2025-06-28 19:14:40 +0300

    1.2

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
 from flask import Flask, session, request, render_template, redirect, url_for
 import os
-from db_script import get_user, add_user
+from db_script import get_user, add_user, get_forums, add_forums, read_forum, add_post, get_posts
+import base64
 
 authorizated = False
+user_id = None
+error_value = "ㅤ"
 
 def index(): 
     if authorizated:
         if  request.method == "GET":
-            print(6)
-            return redirect(url_for("check"))
+            return redirect(url_for("main-page"))
         else:
-            print(7)
-            return redirect(url_for("check"))
+            return redirect(url_for("main-page"))
     else:
         return redirect(url_for("authorization"))
 
-def check():
-    return render_template("check.html")
+def mainpage():
+    global error_value
+    if authorizated == True:
+        if request.method == "POST":
+            if not request.form['create-forum-input'] is None:
+                error_value = add_forums({
+                    "id_user": user_id,
+                    "name": request.form['create-forum-input']
+                })
+                print(error_value)
+                if error_value == None:
+                    error_value = "Такой форум уже существует."
+                return redirect(url_for("main-page"))
+        # request.method = "GET"
+        row = get_forums()
+        print(error_value)
+        if error_value != "ㅤ" and error_value != "Такой форум уже существует.":
+            error_value = "ㅤ"
+        return render_template("main-page.html", forums = row, error = error_value)
+    else:
+        return redirect(url_for("authorization"))
+
 
 
 def authorization():
-    print(1)
     if request.method == "POST":
         login = get_user(request.form["login"], request.form["password"]) 
-        print(login)
         try:
             if login[1] == request.form["login"]:
                 global authorizated
-                print(111)
+                global user_id
+                user_id = login[0]
                 authorizated = True
                 return redirect(url_for("index"))
         except:
@@ -35,26 +55,26 @@ def authorization():
     return render_template("authorization.html")
 
 def authorization_failed():
-    print(1)
     if request.method == "POST":
         login = get_user(request.form["login"], request.form["password"]) 
-        print(login)
         try:
             if login[1] == request.form["login"]:
                 global authorizated
+                global user_id
+                user_id = login[0]
                 authorizated = True
                 return redirect(url_for("index"))
         except:
             return render_template("authorization-failed.html")
     return render_template("authorization-failed.html")
 
+
 def registration():
     if request.method == "POST":
         user = {
             "login": request.form.get("login"),
-            "name": request.form.get("name"),
+            # "name": request.form.get("name"),
             "password": request.form.get("password"),
-            "about": request.form.get("about", ""),
             "image": request.files.get("image")
         }
         add_user(user)
@@ -64,18 +84,47 @@ def registration():
 folder = os.getcwd()
 
 app = Flask(__name__, template_folder="template", static_folder="static")
-@app.errorhandler(400)
-def handle_400(e):
-    return f"Ошибка 400: {e}", 400
+# @app.errorhandler(400)
+# def handle_400(e):
+#     return f"Ошибка 400: {e}", 400
 
+def open_forum():
+    if authorizated == True:
+        forum_id = request.form.get('forum_id') or request.args.get('forum_id')
+        if not forum_id:
+            return "Forum ID missing", 400
+        forum_name = read_forum(forum_id)
+        
+        
+        message = request.form.get('send-message-input')
+        if request.method == "POST":    
+            if not request.form.get('send-message-input') is None:
+                print(message)
+                if not message is None:
+                    send_message(forum_name, message)
+                    return redirect(url_for("open_forum", forum_id=forum_id))
+        rows = get_posts(forum_name[0])
+        return render_template(f"forums/forum-page-{forum_name[0]}-{forum_id}.html", name = forum_name[0], id = forum_id, posts = rows)
+    else:
+        return redirect(url_for("authorization"))
+
+def send_message(forum_name, message):
+    global user_id
+    add_post(forum_name,message,user_id)
+    return redirect(url_for("open_forum"))
+    
+    
+    
+
+    
 app.add_url_rule("/", "index", index, methods = ["post", "get"])
-app.add_url_rule("/check", "check", check, methods = ["post", "get"])
+app.add_url_rule("/main-page", "main-page", mainpage, methods = ["post", "get"])
 app.add_url_rule("/authorization", "authorization", authorization, methods = ["post", "get"])
 app.add_url_rule("/authorization-failed", "authorization-failed", authorization_failed, methods = ["post", "get"])
 app.add_url_rule("/registration", "registration", registration, methods = ["post", "get"])
+app.add_url_rule("/open_forum", "open_forum", open_forum, methods = ["post", "get"])
 
 app.config["SECRET_KEY"] = "DASISTPLAKEPLAKEQUIZ"
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- db_script.py
+++ db_script.py
@@ -1,5 +1,6 @@
 import sqlite3
 from random import randint
+import base64
 
 conn = None
 curs = None
@@ -19,10 +20,9 @@ def do(request):
     
 def clear_db():
     open_db()
-    do("DROP TABLE IF EXISTS quiz_content")
-    do("DROP TABLE IF EXISTS quiz")
-    do("DROP TABLE IF EXISTS question")
     do("DROP TABLE IF EXISTS user")
+    do("DROP TABLE IF EXISTS posts")
+    do("DROP TABLE IF EXISTS forums")
     
 def create():
     open_db()
@@ -30,25 +30,128 @@ def create():
     do("""CREATE TABLE IF NOT EXISTS user(
         id INTEGER PRIMARY KEY,
         login VARCHAR,
-        name VARCHAR,
         password VARCHAR,
-        about VARCHAR,
         photo BLOB)
+        """)
+    do("""CREATE TABLE IF NOT EXISTS posts(
+        id INTEGER PRIMARY KEY,
+        id_user VARCHAR,
+        user_name VARCHAR,
+        photo BLOB,
+        forum VARCHAR,
+        content VARCHAR)
+        """)
+    do("""CREATE TABLE IF NOT EXISTS forums(
+        id INTEGER PRIMARY KEY,
+        id_user VARCHAR,
+        name VARCHAR)
         """)
     close()
     
 def add_user(user): 
-    print(user["image"])
     photo = user["image"].read()
     open_db() 
-    curs.execute("INSERT INTO user (login, name, password, about, photo) VALUES (?,?,?,?,?)", 
-                (user["login"],user["name"],user["password"],user["about"],photo)) 
+    curs.execute("INSERT INTO user (login, password, photo) VALUES (?,?,?)", 
+                (user["login"],user["password"],photo)) 
     conn.commit() 
     close()
+
+def get_forums():
+    open_db()
+    curs.execute("SELECT id, name FROM forums")
+    rows = curs.fetchall()  # [('zz',), ('5z',)]
+    close()
+    return rows
+
+
+def add_post(forum,content,user_id):
+    open_db()
+    user_name = get_user_by_id(user_id)
+    photo = get_user_photo(user_id)
+    curs.execute("INSERT INTO posts (id_user, user_name,photo, forum, content) VALUES (?,?,?,?,?)", 
+                (user_id,user_name,photo,forum[0],content))
+    print(get_user_photo_way(user_id))
+    conn.commit()
+    close()
+
+
+def get_user_by_id(user_id):
+    curs.execute("SELECT login FROM user WHERE id == (?)", (user_id,))
+    user = curs.fetchone()
+    return user[0]
+def get_user_photo(user_id):
+    curs.execute("SELECT photo FROM user WHERE id == (?)", (user_id,))
+    user = curs.fetchone()
+    return user[0]
+
+    
+def get_posts(forum):
+    open_db()
+    curs.execute("SELECT user_name, content, photo FROM posts WHERE forum == (?)", (forum,))
+    rows = curs.fetchall() 
+    close()
+    result = []
+    for user_name, content, photo in rows:
+        if photo:
+            photo_base64 = base64.b64encode(photo).decode('utf-8')
+        else:
+            photo_base64 = None
+        result.append((user_name, content, photo_base64))
+
+    return result
+def get_user_photo_way(user_id):
+    curs.execute("SELECT photo FROM user WHERE id = ?", (user_id,))
+    result = curs.fetchone()
+    if result and result[0]:
+        encoded = base64.b64encode(result[0]).decode('utf-8')
+        
+        return encoded
+    return None
+
+
+# user1 = {
+#         "id_user": 1,
+#         "name": "zz"
+#     }
+# user2 = {
+#         "id_user": 2,
+#         "name": "5z"
+#     }
+def add_forums(forums): 
+    open_db()
+    print(forums["name"])
+    curs.execute("SELECT id FROM forums WHERE name == (?)",
+                 [str(forums["name"])])
+    if curs.fetchone() == None:
+        print(1)
+        curs.execute("INSERT INTO forums (id_user, name) VALUES (?,?)", 
+                    (forums["id_user"],forums["name"]))
+        conn.commit() 
+        curs.execute("SELECT id FROM forums WHERE id_user == (?) AND name == (?)",
+                    [str(forums["id_user"]),str(forums["name"])])
+        id = curs.fetchone()
+        create_forum_page(forums['name'], id[0])
+        close()
+        return True
+    else:
+        return None 
+    
+def create_forum_page(name, index):
+    with open("template/forum.html", "r") as template_file:
+        html_content = template_file.read()
+    with open(f"template/forums/forum-page-{name}-{index}.html", "w") as file:
+        file.write(html_content)
+def read_forum(forum_id):
+    open_db()
+    curs.execute("SELECT name FROM forums WHERE id == (?)",
+                 (forum_id,))
+    forum_name = curs.fetchone()
+    return forum_name
+
+    
     
 def get_user(login, password):
     open_db()
-    # curs.execute("SELECT id, login FROM user WHERE login==(?) AND password==(?)", (login, password))
     curs.execute("SELECT id, login FROM user WHERE login = ? AND password = ?", (login, password))
     result = curs.fetchone()
     close()
@@ -61,4 +164,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-
